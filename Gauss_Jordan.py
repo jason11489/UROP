6e3415e4addb249d6115c253a0c4bdf7c89242cc
jason11489 <75050680+jason11489@users.noreply.github.com>
2021-08-03 14:34:41 +0900

    modify code

--- Gauss_Jordan.py
+++ Gauss_Jordan.py
@@ -15,43 +15,57 @@ def addi_operation(list,a,b,num,p):
     for i in range(len(list[a])):
         sub_num = list[a][i]*num
         list[b][i] = (list[b][i]+sub_num)%p
+        sub_num = 0
 
 def line_multi(list,i,num,p):
-    if num == 0:
-        return
-    for i in range(len(list[i])):
-        list[i] = (list[i]*num)%p
+    for cnt in range(len(list[i])):
+        list[i][cnt] = (list[i][cnt]*num)%p
+
 
 def Gauss_Jordan(list,p):
-    size = len(list)
+    cost = 5
+    total = 5
 
     if list[0][0] == 0:
-        for i in range(size):
+        for i in range(total):
             if list[i][0] != 0:
                 break
         line_change(list,0,i)
-    for i in range(0,size):
-        for j in range(i+1,size):
-            num = find_addition_inverse(list[i][i],list[j][i],p)
-            addi_operation(list,i,j,num,p)
 
-        inverse_num = Lib.inverse(list[i+1][i+1],p)
-        line_multi(list,i+1,inverse_num,p)
+    for i in range(total):
+
+        if list[i][i] == 0:
+            for k in range(i,total):
+                if list[k][i] != 0:
+                    break
+            line_change(list,i,k)
+
+        if list[i][i] != 0:
+            inverse_num = Lib.inverse(list[i][i],p)
+            line_multi(list,i,inverse_num,p)
+            for j in range(i+1,total):
+                if list[j][i] != 0:
+                    num = find_addition_inverse(list[i][i],list[j][i],p)
+                    addi_operation(list,i,j,num,p)
 
 
-    for i in range(0,size):
-        for j in range(i,size):
-            num = find_addition_inverse(list[size-1][size-1-i],list[size-1-j][size-1-i],p)
-            addi_operation(list,size-1-i,size-2-i)
+
+
+    for i in range(0,total):
+        for j in range(i+1,total):
+            num = find_addition_inverse(list[4-i][4-i],list[4-j][4-i],p)
+            addi_operation(list,4-i,4-j,num,p)
 
     final_list = []
     for i in list:
-        final_list.append(i[size])
+        final_list.append(i[cost])
     return final_list
 
 
 
 if __name__ == "__main__":
-    print(1)
+    list = [[2,2,1,0,0,180],[4,0,0,0,1,176],[0,1,1,0,1,165],[1,0,0,1,1,154],[1,2,0,0,1,198],[1,1,1,1,0,210]]
+    sol_list = Gauss_Jordan(list,228)
+    print(sol_list)
 
 
--- Index_Calculus.py
+++ Index_Calculus.py
@@ -8,14 +8,15 @@ import Gauss_Jordan
 
 def Index_Calculus(p,q,g,h):   # q|(p-1) ( p,q : prime ), H = <g> , | H | = q , find x s.t. g^x = h
     cost = 5
+    total = 5
     S = []
     equation_list = []
-    for i in range(cost):
+    for i in range(total):
         equation_list.append([0 for i in range(cost+1)])
     S.append(2)
     S.append(3)
     S.append(5)
-    for i in range(cost-3):
+    for i in range(total-3):
         while 1:
             prime = Miller_Rabin.Probably_Prime(2,p-1)
             if prime not in S:
@@ -26,7 +27,7 @@ def Index_Calculus(p,q,g,h):   # q|(p-1) ( p,q : prime ), H = <g> , | H | = q , 
     cnt = 0
     k = 0
     list_k = []
-    while cnt<cost:
+    while cnt<total:
 
         while 1:
             while 1:
@@ -58,14 +59,38 @@ def Index_Calculus(p,q,g,h):   # q|(p-1) ( p,q : prime ), H = <g> , | H | = q , 
     print(1)
     X_sol = Gauss_Jordan.Gauss_Jordan(equation_list,q)
 
+    while 1:
+        random_k = random.randint(2,q-1)
+        h_g_k = (h*Lib.mns(g,random_k,p))%p
+
+        divisor_h_g_k = Lib.divisor(h_g_k)
+        num_divisor = 0
+        for i in divisor_h_g_k:
+            if i in S:
+                num_divisor += 1
+
+        if num_divisor == len(divisor_h_g_k):
+            break
+    h_g_k_factor = Lib.factor(h_g_k)
+    final_x = 0
+    for i in h_g_k_factor:
+        num_S = S.index(i[0])
+        X_j = X_sol[num_S]
+        final_x = final_x + X_j*i[1]
+
+    final_x = final_x - random_k
+    if final_x<0:
+        final_x += q
+    return final_x
+
 
 
 if __name__ == "__main__":
-    #p,q,g = DSA_Parameter.DSA_Parameter(40,200)
-    p,q,g = 83,41,10
-    x = random.randint(2,q)
+    p,q,g = DSA_Parameter.DSA_Parameter(10,600)
+    x = random.randint(2,q-1)
     h = Lib.mns(g,x,p)
 
-    Index_Calculus(p,q,g,h)
+    final_x = Index_Calculus(p,q,g,h)
+    print("real x = {}\nsol x = {}".format(x,final_x))
 
 
